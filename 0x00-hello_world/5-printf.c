060c1c8b4766bbc9138dfd84aab6accc2ff20f2a
HIRWA Junior <iamhirwejr@gmail.com>
2022-12-08 22:30:04 +0000

    commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,12 @@
-#include<stdio.h>
+#include <stdio.h>
 
+/**
+ *
+ * main program 
+ *     
+ *     */
 int main(void)
 {
-	printf("with proper grammar, but the outcomeis a piece of art\n");
-		return 0;
+		printf("with proper grammar, but the outcome is a piece of art,\n");
+			return (0);
 }
