2b854c0d34b0b36122bccd6bee611014949d5592
HIRWA Junior <iamhirwejr@gmail.com>
2022-12-12 12:42:29 +0000

    print all numbers of base 10 using putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+  * main - Entry point
+  *
+  * Return: Always 0 (Success)
+  */
+int main(void)
+{
+	char nums;
+
+	for (nums = '0'; nums <= '9'; nums++)
+		putchar(nums);
+	putchar('\n');
+
+	return (0);
+}
