e85e06756e88195adbbb77fd6ba40a237051fc05
HIRWA Junior <iamhirwejr@gmail.com>
2022-12-12 16:20:45 +0000

    print numbers separated with , and ends with $

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+  * main -Entry point
+  *
+  * Return: Always 0 (Success)
+  */
+int main(void)
+{
+	int num;
+
+	for (num = '0'; num <= '9'; num++)
+	{
+		putchar(num);
+		if (num != '9')
+			putchar(',');
+		else
+			putchar('$');
+	}
+	putchar('\n');
+
+	return (0);
+}
